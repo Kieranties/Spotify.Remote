a67cdd80c3cef8dcf9ca64bd271786e9c37211f2
Kieranties <Kieran@Kieranties.com>
2014-02-25 20:29:32 +0000

    trivial home page

---
+++ service/api/controllers/HomeController.js
@@ -0,0 +1,45 @@
+/**
+ * HomeController
+ *
+ * @module      :: Controller
+ * @description    :: A set of functions called `actions`.
+ *
+ *                 Actions contain code telling Sails how to respond to a certain type of request.
+ *                 (i.e. do stuff, then send some JSON, show an HTML page, or redirect to another URL)
+ *
+ *                 You can configure the blueprint URLs which trigger these actions (`config/controllers.js`)
+ *                 and/or override them with custom routes (`config/routes.js`)
+ *
+ *                 NOTE: The code you write here supports both HTTP and Socket.io automatically.
+ *
+ * @docs        :: http://sailsjs.org/#!documentation/controllers
+ */
+
+module.exports = {
+
+
+    /**
+     * Action blueprints:
+     *    `/home/index`
+     *    `/home`
+     */
+    index: function (req, res) {
+        console.log('hello from home');
+        res.view({
+            title: 'Weddify',
+            css: ['/css/home.css']
+        });
+    },
+
+
+    /**
+     * Overrides for the settings in `config/controllers.js`
+     * (specific to HomeController)
+     */
+    _config: {
+        actions: false,
+        rest: false
+    }
+
+
+};
