050e132644954bca3da320f677c5821f29133cf2
Kieranties <Kieran@Kieranties.com>
2014-03-03 20:39:50 +0000

    All basic searches done

--- service/assets/scripts/search.js
+++ service/assets/scripts/search.js
@@ -1,6 +1,7 @@
 $(function(){
     var templates = {
-        simpleResult: Handlebars.compile($("#simple-result").html())
+        simpleResult: Handlebars.compile($("#simple-result").html()),
+        trackRow: Handlebars.compile($("#track-row").html())
     };
 
     var getArtwork = function(query, callback){
@@ -10,6 +11,28 @@ $(function(){
         });
     }
 
+    var searchTracks = function(query, pg){
+        var uri = 'http://ws.spotify.com/search/1/track.json';
+        var $wrap = $('#track-results');
+
+        $.getJSON(uri,{q: query, page: pg}, function(resp){
+            $('.result-count', $wrap).text(resp.info.num_results);
+            var $resultList = $('.result-list', $wrap);
+            resp.tracks.forEach(function(track, i){
+                getArtwork(track.href, function(artwork){
+                    $resultList.append(templates.trackRow({
+                        href: track.href,
+                        name: track.name,
+                        artwork: artwork.replace('cover','60'),
+                        count: resp.offset + i,
+                        artistName: track.artists[0].name,
+                        albumName: track.album.name
+                    }));
+                })
+
+            });
+        })
+    }
     var searchArtist = function(query, limit){
         var uri = 'http://ws.spotify.com/search/1/artist.json';
         var $wrap = $('#artist-results');
@@ -51,9 +74,12 @@ $(function(){
     }
 
     var doSearch = function(){
-        var query = $('#search').val() + '*';
+        $('.result-count, .result-list').empty();
+        var query = $('#search').val();
         searchArtist(query, 3);
         searchAlbum(query, 3);
+        searchTracks(query, 1);
+        return false;
     }
 
     $('#searchForm').on('submit', doSearch);
